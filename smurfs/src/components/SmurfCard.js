fa728360e0cb7104d1402eefc52671e1e53212e0
Tyler <nuvallo.inc@gmail.com>
2020-02-08 13:39:04 -0500

    Styling cards and form

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -4,10 +4,14 @@ import { Card, CardTitle, CardText } from "reactstrap";
 export default function SmurfCard(props) {
   console.log("SmurfCard: ", props);
   return (
-    <Card>
-      <CardTitle>{props.smurf.name}</CardTitle>
-      <CardText>Age: {props.smurf.age}</CardText>
-      <CardText>Height: {props.smurf.height}</CardText>
+    <Card className="card">
+      <CardTitle className="card-title">{props.smurf.name}</CardTitle>
+      <CardText>
+        <span className="bold">Age:</span> {props.smurf.age}
+      </CardText>
+      <CardText>
+        <span className="bold">Height:</span> {props.smurf.height}
+      </CardText>
     </Card>
   );
 }
