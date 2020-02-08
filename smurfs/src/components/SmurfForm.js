64b9251e1b74af8bd2479b9564de13ecf51fc3c3
Tyler <nuvallo.inc@gmail.com>
2020-02-08 12:35:38 -0500

    Axios and State imported into form component, handlers are created and working

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Container, Col, Form, FormGroup, Input, Button } from "reactstrap";
 
 export default function SmurfForm() {
+  const [newSmurf, setNewSmurf] = useState({});
+
+  const handleChange = event => {
+    setNewSmurf({ ...newSmurf, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = () => {
+    axios.post("http://localhost:3333/smurfs", newSmurf).then(res => {
+      console.log(res);
+    });
+  };
+
   return (
     <Container className="App">
-      <Form className="form">
+      <Form onSubmit={handleSubmit} className="form">
         <Col>
           <FormGroup>
-            <Input type="text" name="name" id="name" placeholder="Name" />
+            <Input
+              type="text"
+              name="name"
+              id="name"
+              value={newSmurf.name}
+              placeholder="Name"
+              onChange={handleChange}
+            />
           </FormGroup>
         </Col>
 
         <Col>
           <FormGroup>
-            <Input type="text" name="age" id="age" placeholder="Age" />
+            <Input
+              type="text"
+              name="age"
+              id="age"
+              value={newSmurf.age}
+              placeholder="Age"
+              onChange={handleChange}
+            />
           </FormGroup>
         </Col>
 
         <Col>
           <FormGroup>
-            <Input type="text" name="height" id="height" placeholder="Height" />
+            <Input
+              type="text"
+              name="height"
+              id="height"
+              value={newSmurf.height}
+              placeholder="Height"
+              onChange={handleChange}
+            />
           </FormGroup>
         </Col>
 
