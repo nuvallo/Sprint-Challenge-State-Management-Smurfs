e86ac3b3ade1cce28ec508d1a0ec1ba6db610889
Tyler <nuvallo.inc@gmail.com>
2020-02-08 12:18:58 -0500

    axios setup and setting up state for smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-class App extends Component {
-  render() {
-    return (
+import { SmurfContext } from "../contexts/SmurfContext";
+
+function App() {
+  const [smurfs, setSmurfs] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3333/smurfs").then(res => {
+      console.log(res.data);
+      setSmurfs(res.data);
+    });
+  }, []);
+
+  return (
+    <SmurfContext.Provider value={{ smurfs }}>
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
       </div>
-    );
-  }
+    </SmurfContext.Provider>
+  );
 }
 
 export default App;
